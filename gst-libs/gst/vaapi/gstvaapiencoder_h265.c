11b763f38743dc2715ee82e8283ef4fbf0bc5f64
He Junyan <junyan.he@hotmail.com>
2020-03-05 17:29:41 +0800

    libs: encoder: h265: promote level if tile is enabled.

--- gst-libs/gst/vaapi/gstvaapiencoder_h265.c
+++ gst-libs/gst/vaapi/gstvaapiencoder_h265.c
@@ -178,6 +178,12 @@ h265_get_slice_type (GstVaapiPictureType type)
   return -1;
 }
 
+static gboolean
+h265_is_tile_enabled (GstVaapiEncoderH265 * encoder)
+{
+  return encoder->num_tile_cols * encoder->num_tile_rows > 1;
+}
+
 /* Get log2_max_pic_order_cnt value for H.265 specification */
 static guint
 h265_get_log2_max_pic_order_cnt (guint num)
@@ -1126,6 +1132,7 @@ ensure_tier_level (GstVaapiEncoderH265 * encoder)
   guint i, num_limits, PicSizeInSamplesY;
   guint LumaSr;
   const GstVaapiH265LevelLimits *limits_table;
+  const GstVaapiH265LevelLimits *limits;
 
   PicSizeInSamplesY = encoder->luma_width * encoder->luma_height;
   LumaSr =
@@ -1134,7 +1141,7 @@ ensure_tier_level (GstVaapiEncoderH265 * encoder)
 
   limits_table = gst_vaapi_utils_h265_get_level_limits_table (&num_limits);
   for (i = 0; i < num_limits; i++) {
-    const GstVaapiH265LevelLimits *const limits = &limits_table[i];
+    limits = &limits_table[i];
     /* Choose level by luma picture size and luma sample rate */
     if (PicSizeInSamplesY <= limits->MaxLumaPs && LumaSr <= limits->MaxLumaSr)
       break;
@@ -1142,6 +1149,23 @@ ensure_tier_level (GstVaapiEncoderH265 * encoder)
 
   if (i == num_limits)
     goto error_unsupported_level;
+
+  /* promote the level by tile setting */
+  if (h265_is_tile_enabled (encoder)) {
+    for (; i < num_limits; i++) {
+      limits = &limits_table[i];
+      if (encoder->num_tile_cols <= limits->MaxTileColumns &&
+          encoder->num_tile_rows <= limits->MaxTileRows)
+        break;
+    }
+
+    if (i == num_limits) {
+      GST_WARNING
+          ("can not find level for num-tile-cols is %d, num-tile-rows %d",
+          encoder->num_tile_cols, encoder->num_tile_rows);
+      goto error_unsupported_level;
+    }
+  }
 
   if (bitrate <= limits_table[i].MaxBRTierMain) {
     encoder->tier = GST_VAAPI_TIER_H265_MAIN;
