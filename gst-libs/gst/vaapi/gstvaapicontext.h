2f71f21937ea74fe0de387412eec617a13df484a
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2020-05-21 21:01:38 +0200

    libs: context: receive preferred format via ContextInfo
    The internal decoder can set the preferred format, from the negotiated
    downstream caps, to the context though the ContextInfo structure.
    
    If the format is set to UNKNOWN the old way to create surfaces is used.
    This is mostly to keep backward compatibility with JPEG decoder for i965
    driver.

--- gst-libs/gst/vaapi/gstvaapicontext.h
+++ gst-libs/gst/vaapi/gstvaapicontext.h
@@ -86,6 +86,7 @@ struct _GstVaapiContextInfo
   GstVaapiProfile profile;
   GstVaapiEntrypoint entrypoint;
   GstVaapiChromaType chroma_type;
+  GstVideoFormat preferred_format;
   guint width;
   guint height;
   guint ref_frames;
@@ -115,7 +116,6 @@ struct _GstVaapiContext
   GstVaapiVideoPool *surfaces_pool;
   gboolean reset_on_resize;
   GstVaapiConfigSurfaceAttributes *attribs;
-  GstVideoFormat preferred_format;
 };
 
 #define GST_VAAPI_CONTEXT_ID(context)        (((GstVaapiContext *)(context))->object_id)
